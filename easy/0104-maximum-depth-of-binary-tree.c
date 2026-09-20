aac89696d2d648534bebff9233f6085dfada8944
Tolga Bilgis <155099070+TolgaBilgis@users.noreply.github.com>
2026-09-19 21:33:57 -0400

    Add maximum depth of binary tree

---
+++ easy/0104-maximum-depth-of-binary-tree.c
@@ -0,0 +1,18 @@
+#include <stddef.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+int maxDepth(struct TreeNode* root) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    int leftDepth = maxDepth(root->left);
+    int rightDepth = maxDepth(root->right);
+
+    return (leftDepth > rightDepth ? leftDepth : rightDepth) + 1;
+}
